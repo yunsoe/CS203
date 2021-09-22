b8401bb50f2208f4dbd0d45ccff1b046d170e646
reginachua272 <regina.chua.2020@smu.edu.sg>
2021-09-22 14:16:05 +0800

    sql2

--- sql/cs203.sql
+++ sql/cs203.sql
@@ -1,2 +1,35 @@
 create schema cs203;
 use cs203;
+create table alert
+(
+
+);
+
+create table company
+(
+);
+create table user
+(
+);
+
+create table event
+(
+);
+
+create table feedback
+(
+);
+
+create table industry
+(
+);
+
+create table news
+(
+);
+
+create table swabTest
+(
+};
+
+
