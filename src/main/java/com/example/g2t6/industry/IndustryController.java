f4a800c679885b818868ae919f80dc58dcf2963f
sarahorhogan <80668328+sarahorhogan@users.noreply.github.com>
2021-09-22 13:12:57 +0800

    Added Industry and Event files

---
+++ src/main/java/com/example/g2t6/industry/IndustryController.java
@@ -0,0 +1,76 @@
+package com.example.g2t6.industry;
+
+import java.util.List;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class IndustryController {
+
+    private IndustryService industryService;
+
+    public IndustryController(IndustryService is){
+        this.industryService = is;
+    }
+
+    /**
+     * List all industries in the system
+     * @return list of all industries
+     */
+    @GetMapping("/industries")
+    public List<Industry> getIndustries(){
+        return industryService.listIndustries();
+    }
+
+    /**
+     * Search for industry with the given id
+     * If there is no industry with the given "id", throw a IndustryNotFoundException
+     * @param id
+     * @return industry with the given id
+     */
+    @GetMapping("/industries/{id}")
+    public Industry getIndustry(@PathVariable Long id){
+        Industry industry = industryService.getIndustry(id);
+
+        // Need to handle "industry not found" error using proper HTTP status code
+        // In this case it should be HTTP 404
+        if(industry == null) throw new IndustryNotFoundException(id);
+        return industryService.getIndustry(id);
+
+    }
+    /**
+     * Add a new industry with POST request to "/industries"
+     * Note the use of @RequestBody
+     * @param industry
+     * @return list of all industries
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/industries")
+    public Industry addIndustry(@RequestBody Industry industry){
+        return industryService.addIndustry(industry);
+    }
+
+    /**
+     * Remove a industry with the DELETE request to "/industries/{id}"
+     * If there is no industry with the given "id", throw a IndustryNotFoundException
+     * @param id
+     */
+    @DeleteMapping("/industries/{id}")
+    public void deleteIndustry(@PathVariable Long id){
+        try{
+            industryService.deleteIndustry(id);
+         }catch(EmptyResultDataAccessException e) {
+            throw new IndustryNotFoundException(id);
+         }
+    }
+    
+}
