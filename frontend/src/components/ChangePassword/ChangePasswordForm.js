440320d1269a1c576dc6ccc7e68977d361aabdab
Lee Zhi Xuan <zhixuan.lee.2020@scis.smu.edu.sg>
2021-10-10 18:50:15 +0800

    update access token after changing password

--- frontend/src/components/ChangePassword/ChangePasswordForm.js
+++ frontend/src/components/ChangePassword/ChangePasswordForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { API_BASE_URL } from "../../constants/apiConstants";
 import { useHistory } from "react-router-dom";
 import { Form, Button, FormGroup } from "react-bootstrap";
+import AuthContext from "../../navigation/AuthContext";
 
 export default function ChangePasswordForm() {
     const history = useHistory();
@@ -19,11 +20,15 @@ export default function ChangePasswordForm() {
         }));
     };
 
+    function createBasicAuthToken(email, password) {
+        return 'Basic ' + window.btoa(email + ":" + password);
+    }
+
     const redirectToHome = () => {
         history.push("/home");
     };
 
-    const changePassword = (e) => {
+    const changePassword = (e, updateAuth) => {
         e.preventDefault();
         fetch(
             API_BASE_URL + "users/" + localStorage.getItem("email") + "/changePassword", 
@@ -42,6 +47,8 @@ export default function ChangePasswordForm() {
         ).then(function (response) {
             console.log(response.status);
             if (response.status === 200) {
+                var token = createBasicAuthToken(localStorage.getItem("email"), state.newPassword);
+                updateAuth(true, localStorage.getItem("email"), localStorage.getItem("authority"), token);
                 alert("Your password has been updated successfully. You will now be redirected back to the home page.");
                 redirectToHome();
             } else if (response.status === 401) {
@@ -56,17 +63,21 @@ export default function ChangePasswordForm() {
             <div className="card col-5 login-card mt-2 hv-center" style={{padding: 20}}>
                 <h3>Change Password</h3>
                 <br/>
-                <Form onSubmit = {(e) => changePassword(e)}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Current Password:</Form.Label>
-                        <Form.Control required type="password" placeholder="Enter current password" onChange={handleChange} id="currentPassword" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>New Password:</Form.Label>
-                        <Form.Control required minLength={8} type="password" placeholder="Enter new password" onChange={handleChange} id="newPassword" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" style={{marginBottom: 10}}>Change Password</Button>
-                </Form>
+                <AuthContext.Consumer>
+                    {({ updateAuth }) => (
+                        <Form onSubmit = {(e) => changePassword(e, updateAuth)}>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Current Password:</Form.Label>
+                                <Form.Control required type="password" placeholder="Enter current password" onChange={handleChange} id="currentPassword" />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>New Password:</Form.Label>
+                                <Form.Control required minLength={8} type="password" placeholder="Enter new password" onChange={handleChange} id="newPassword" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" style={{marginBottom: 10}}>Change Password</Button>
+                        </Form>
+                    )}
+                </AuthContext.Consumer>
             </div>
         </div>
     );
