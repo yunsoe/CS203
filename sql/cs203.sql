3ad572b7197c79480f2873d3466b673674d3ca37
charlotte-w-t <twee.2020@scis.smu.edu.sg>
2021-10-10 20:07:16 +0800

    edited cs203.sql

--- sql/cs203.sql
+++ sql/cs203.sql
@@ -1,33 +1,50 @@
-create schema cs203;
+create schema if not exists cs203;
 use cs203;
+
+drop table if exists `alert`;
 create table alert
 (
-
+    -- aid int not null primary key,
+    -- alertDate DateTime not null,
+    -- alert varchar(255) not null
 );
 
+drop table if exists 'company'
 create table company
 (
+    -- company_id int not null primary key,
+    -- cname varchar(255)
 );
+
+drop table if exists 'user'
 create table user
+    -- user_email varchar(255) not null primary key,
+    -- uname varchar(255) not null,
+    -- pword varchar(255) not null
 (
 );
 
+drop table if exists 'event'
 create table event
 (
 );
 
+drop table if exists 'feedback'
 create table feedback
 (
 );
 
+drop table if exists 'industry'
 create table industry
 (
 );
 
+drop table if exists 'news'
 create table news
 (
 );
 
+drop table if exists 'swabtest'
 create table swabTest
 (
 };
