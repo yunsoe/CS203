3e436739bf7b4cd461bea8f24c87021b87613e04
reginachua272 <regina.chua.2020@smu.edu.sg>
2021-10-10 17:11:04 +0800

    swabTest frontend

--- frontend/src/components/swabTest/SwabTestForm.js
+++ frontend/src/components/swabTest/SwabTestForm.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
-import { Button,InputGroup,FormControl } from "react-bootstrap";
-
+import { Button,InputGroup,Form,Col } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {format} from "date-fns";
 export default function SwabTestForm() {
     return(
-        <div>
-        <div>SwabTest</div>
-       
+        <div style={{display: "flex", justifyContent: "center", marginTop: 200}}>
+        <br/>
+        <Form> 
+        <Col>
+        <Form.Label>swab Result:</Form.Label>
+        </Col>
+         <Col>
+        <select>
+            <option value="true">positive</option>
+            <option selected value="false">negative</option>
+        </select>
+        </Col>
+             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+             <Col>
+             <Form.Label>date of swab:</Form.Label>
+             </Col>
+             <Col>
+             <DatePicker wrapperClassName="datePicker" className="actualDate" />
+             </Col>
+            </Form.Group>
+        </Form>
         </div>
     );
 }
--- frontend/src/components/swabTest/SwabTestHistory.js
+++ frontend/src/components/swabTest/SwabTestHistory.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState,useEffect } from "react";
-import { Button,InputGroup,FormControl } from "react-bootstrap";
+import { Button,InputGroup,FormControl, Col, Card,ListGroup } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import {format} from "date-fns";
@@ -9,21 +9,43 @@ import {format} from "date-fns";
 export default function SwabTestForm() {
 
     const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
     const [data, setData] = useState([]);
 
 
-    const handleChange = (date) => {
+    const handleChange1 = (date) => {
         setStartDate(date)
     }
 
+    const handleChange2 = (date) => {
+        setEndDate(date)
+      }
+
     const handleSubmit = ()=> {
+        
         console.log("enter");
     }
 
-    function loadDate(){
-        const today = format(new Date(), 'yyyy-MM-dd')
-        return axios.get('')
+    const item = (todo) => (
+        <ListGroup.Item key={data.id}>
+          <Card>
+            <Card.Body>
+              <Card.Text>{data.swabResult}</Card.Text>
+              <Card.Subtitle>{data.actaulSwabTestResult}</Card.Subtitle>
+            </Card.Body>
+          </Card>
+        </ListGroup.Item>
+      );
+
+    function loadData(){
+        axios.get('/swabTest/{startDate}/date/{endDate}').then(response=>{
+            setData(response.data)
+        })
     }
+
+    useEffect(()=> {
+        loadData();
+    },[]);
 
     return(
         <div>
@@ -34,11 +56,30 @@ export default function SwabTestForm() {
             aria-label="swabTesHitory"
             aria-describedby="basic-addon2"
             />
-            <DatePicker wrapperClassName="datePicker" className="form-control" selected={startDate} onChange={handleChange} />
             <Button variant="outline-secondary" id="button-addon2" onClick={handleSubmit}>
-            Enter
+                Enter
             </Button>
         </InputGroup>
+            <Col>
+                <DatePicker wrapperClassName="datePicker" className="startDate" selected={startDate} onChange={handleChange1} />
+            </Col>
+            <Col>
+                <DatePicker wrapperClassName="datePicker" className="endDate" selected={endDate} onChange={handleChange2} />
+            </Col>
+            <Col>
+            <Button variant="outline-secondary" id="button-addon2" onClick={handleSubmit}>
+                Enter
+            </Button>
+            </Col>
+            <Card className="to-do-well">
+              <Card.Body>
+                <Card.Title><h2>Swab Results</h2></Card.Title>
+                <ListGroup>
+                  { data.map(n => item(n)) }
+                </ListGroup>
+              </Card.Body>
+            </Card>
+            
         </div>
     );
 }
