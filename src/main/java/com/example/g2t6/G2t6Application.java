0d9fed9f007525995a8198276b11ddcc06f16b90
yunsoe <yunsoe3105@gmail.com>
2021-10-16 20:19:58 +0800

    Update G2t6Application.java

--- src/main/java/com/example/g2t6/G2t6Application.java
+++ src/main/java/com/example/g2t6/G2t6Application.java
@@ -5,33 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class G2t6Application {
-	/*
-	private static final Logger LOGGER = LoggerFactory.getLogger(Week3Application.class);
-    private static BookClient client;
-	*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(G2t6Application.class, args);
 
 	}
-	/*
-	@Scheduled(fixedRateString = "${someJob.rate}")
-	void getBooks() throws InterruptedException {
-        // Our web service endpoint
-        String URI = "http://localhost:8080/users";
-
-        // Perform a GET request
-        Book book = client.getUser(URI, 1L);
-        LOGGER.info("[RestTemplate] GET user: " + user.getEmail());
-        
-	}
-	*/
 
 }
 
