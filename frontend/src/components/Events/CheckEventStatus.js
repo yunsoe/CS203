5effaf71f549cbde26ddf2062949763c089d171f
sarahorhogan <80668328+sarahorhogan@users.noreply.github.com>
2021-11-07 01:41:39 +0800

    event UI

--- frontend/src/components/Events/CheckEventStatus.js
+++ frontend/src/components/Events/CheckEventStatus.js
@@ -44,13 +44,14 @@ export default function CheckEventStatus() {
                     <h3>Event Status</h3>
                     <br/>
                     {status ? 
-                        <h4 style = {{fontFamily:"Arial"}}> <em style = {{color: "red", fontFamily:"Arial"}}>Number of attendees who tested Covid-positive : {status}.</em>
+                        <h4 style = {{fontFamily:"Arial"}}> <em style = {{color: "red", fontFamily:"Arial"}}>
+                        Number of attendees who tested Covid-positive : {status}.</em>
                         <br></br>
                         <br></br>
                         Hence, it would be advisable for you to <strong> do a swab test<br></br> as soon as possible 
                         and self-quarantine if necessary</strong>. <br></br>As a precaution, please unregister from all upcoming<br></br> events
                         until you've taken a swab test and tested negative.</h4>
-                    : <h4 style = {{fontFamily:"Arial"}}> None of the attendees tested Covid-Positive.</h4>}
+                    : <h4 style = {{fontFamily:"Arial"}}> This event took place more than 2 weeks ago/None of the attendees tested Covid-Positive.</h4>}
                 </div>
             );
         
